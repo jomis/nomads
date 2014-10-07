df1062fb60a1c741d473b59e3c0deb1df1132bb7
Johannes M. Schleicher <johannes.schleicher@gmail.com>
2014-10-07 18:47:37 +0200

    Added nomads service unit for fleet  * Fixed etcd.sh

--- docker/nomads_container/etcd.sh
+++ docker/nomads_container/etcd.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 URL=https://discovery.etcd.io/6fbe42763b9e1799802ce72ac8b5159e
-exec /opt/etcd-v0.4.6-linux-amd64/etcd --discovery ${URL} 2>&1
+exec /opt/etcd-v0.4.6-linux-amd64/etcd --discovery ${URL} --peer-addr ${PUBLIC_IP}:7002 --peer-bind-addr 127.0.0.1:7002 2>&1
 
